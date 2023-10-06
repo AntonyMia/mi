1e788893f7c5269a48db3eb91a4b8da5958688ee
Michal Niedzwiadek <mniedzwiadek00@gmail.com>
2023-10-06 09:43:31 +0200

    created js changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-const nav = document.querySelector('.nav'); /*teraz jak wstawimy zmienną nav, to będzie ona oznaczała całego diva z klasą nav*/
+const nav =
+	document.querySelector(
+		'.nav'
+	); /*teraz jak wstawimy zmienną nav, to będzie ona oznaczała całego diva z klasą nav*/
 const navBtn = document.querySelector('.burger-btn');
-function handleNav () {
-    nav.classList.toggle('nav--active')
-}
-navBtn.addEventListener('click',handleNav)
+const allNavItems = document.querySelectorAll('nav__item');
+const handleNav = () => {
+	nav.classList.toggle('nav--active');
+};
+navBtn.addEventListener('click', handleNav);
