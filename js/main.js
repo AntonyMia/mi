314cc2bafbe3460437b2727700f14eb6cf2eca2e
Michal Niedzwiadek <mniedzwiadek00@gmail.com>
2023-11-07 10:05:03 +0100

    styled cards, card

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const footer = document.querySelector('.footer');
 const navMobile =
 	document.querySelector(
 		'.nav-mobile'
@@ -13,8 +14,19 @@ const handleCurrentYear = () => {
 };
 handleCurrentYear();
 
-/*piszemy funkcję, która będzie wykrywała zdarzenie klika na nasz przycisk, i jak wykryje tego klika, to się wykona funkcja handleNav. Teraz nad kodem "navBtn.addEventListener('click', handleNav)"" stworzymy sobie tę funkcję handleNav. Ta funkcja handleNav będzie dodawała i odejmowała klasę .is-active w burgerze - to o czym mówili w instruktarzu dodawania gotowego hemburgera, jako krok czwarty na stronie https://github.com/jonsuh/hamburgers */
+/*piszemy funkcję, która będzie wykrywała zdarzenie klika na nasz przycisk, i jak wykryje tego klika, to się wykona funkcja handleNav. Teraz nad kodem "navBtn.addEventListener('click', handleNav)"" stworzymy sobie tę funkcję handleNav. Ta funkcja handleNav będzie dodawała i odejmowała klasę .is-active w burgerze - to o czym mówili w instruktarzu dodawania gotowego hemburgera, jako krok czwarty na stronie https://github.com/jonsuh/hamburgers ; linijka: navMobile.classList.toggle('nav-mobile--active') natomiast dodaja/odejmuje do elementu z klasą  .nav-mobile, który jest przedstawiony za pomocą zmiennej navMobile, klasę nav-mobile-active. Ta klasa powoduje, element z tą klasą będzie miał pozycje 0 na osi X, czyli powróci z zaświatów  - to widać na linijce 123 main: &--active {
+				transform: translateX(0%);
+			}*/
 handleNav = () => {
-	navBtn.classList.toggle('is-active');
+	navBtn.classList.toggle(
+		'is-active'
+	); /*uwaga, tu nie dodajemy kropki, jak na górze*/
+	navMobile.classList.toggle('nav-mobile--active');
+	if (navMobile.classList.contains('nav-mobile--active')) {
+		/*fragment kodu mający na celu ukrycie footera podczas gdy pojawia się klasa 'nav-mobile--active'*/
+		footer.style.display = 'none';
+	} else {
+		footer.style.display = 'block';
+	}
 };
 navBtn.addEventListener('click', handleNav);
