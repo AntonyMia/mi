cb383162c0f6f65fe70ec782d719118a06d10199
Michal Niedzwiadek <mniedzwiadek00@gmail.com>
2023-12-24 13:11:23 +0100

    new changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-const heading = document.querySelector('h1');
-console.log(heading);
-const par = document.querySelectorAll('p');
-console.log(par);
-const monkey = document.querySelector('.test');
-console.log(monkey);
-const monkey1 = monkey.querySelector('#test');
-console.log(monkey1);
-const apple = document.createElement('ul');
-console.log(object);
+const test1 = document.querySelector('div.yellow');
+console.log(test1);
+console.log(test1.innerText);
+test1.innerText = '<div> antisocial</div>';
+console.log(test1.innerText);
+test2 = document.querySelector('p.inner-text');
+console.log(test2);
+test3 = document.querySelector('p.text-content');
+console.log(test3);
+console.log(test2.innerText);
+console.log(test3.textContent);
+test4 = document.querySelector('p.young');
+console.log(test4);
+console.log(test4.textContent);
+test4.textContent = ' - Taka była wola większości na walnym zgromadzeniu. Część wędkarzy od dłuższego czasu .';
+console.log(test4.textContent);
+const input = document.querySelector('input');
+const underInput = document.querySelector('.under-input')
