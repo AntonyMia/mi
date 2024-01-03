9b18044cf08574f5c6493fd085956944d2ee310e
Michal Niedzwiadek <mniedzwiadek00@gmail.com>
2024-01-03 15:38:40 +0100

    new stuff

--- js/main.js
+++ js/main.js
@@ -1,30 +1,11 @@
-const lime = document.querySelector('.lime');
-const blue = document.querySelector('.blue');
-const gold = document.querySelector('.gold');
-const orange = document.querySelector('.orange');
-const red = document.querySelector('.red');
-const infoRed = () => {
-	console.log('%ckod, który jest supernadrzędny', 'color: red; text-transform: uppercase');
+const test = () => {
+	console.log(`dupa`);
 };
-const infoOrange = () => {
-	// e.stopPropagation();
-	console.log('%ckod, który jest nadrzędny', 'color: orange; text-transform: uppercase');
+setTimeout(test, 5000);
+const test2 = () => {
+	for (let i = 0; i < 5; i++) {
+		console.log(`Liczba do przepracowania to wciąż ${i} dni`);
+	}
 };
-const infoLime = () => {
-	// e.stopPropagation();
-	console.log('%ckod, którego nie chcemy wykonać nr2', 'color: lime; text-transform: uppercase');
-};
-
-const infoBlue = () => {
-	// e.stopPropagation();
-	console.log('%ckod, którego nie chcemy wykonać nr1', 'color: royalblue; text-transform: uppercase');
-};
-const infoGold = () => {
-	// e.stopPropagation();
-	console.log('%c chcemy wykonać ten kod', 'color: gold; text-transform: uppercase');
-};
-red.addEventListener('click', infoRed);
-orange.addEventListener('click', infoOrange);
-lime.addEventListener('click', infoLime);
-blue.addEventListener('click', infoBlue);
-gold.addEventListener('click', infoGold);
+test2();
+setTimeout(test2, 1000);
