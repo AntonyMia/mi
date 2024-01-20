7d550335631aab17d1851fad010f8c3252c101ab
Michal Niedzwiadek <mniedzwiadek00@gmail.com>
2024-01-20 15:20:29 +0100

    new project

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-const pass = document.querySelector('#password');
-const p = document.querySelector('.passinfo');
-const letters = /[a-z]/i;
-const numbers = /[0-9]/;
-const special = /[!@#$%^&*()]/;
-const minValue = 10;
+const burgerBtn = document.querySelector('.burger');
+const barsIco = document.querySelector('.fa-bars');
+const xIco = document.querySelector('.fa-xmark');
+const nav = document.querySelector('nav ul');
+const handleNav = () => {
+	nav.classList.toggle('active');
+	burgerBtn.classList.toggle('active');
+	barsIco.classList.toggle('hide');
+	xIco.classList.toggle('hide');
+};
+burgerBtn.addEventListener('click', handleNav);
